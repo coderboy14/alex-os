73a58c04e95aa22d7a996316b7e5e57591634eba
Alex Wolfe <manziethan11@gmail.com>
2022-03-09 04:27:35 -0500

    Got the code to boot

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -5,11 +5,27 @@
 #include "system.h"
 #include "vga.h"
 #include "ata.h"
+#include "pmm.h"
+#include "kheap.h"
+#include "pci.h"
 
 void kernel_main(void)  {
     vga_initialize();
-    //vga_writestring("Starting system...\n");
-    disk_test();
+    vga_writestring("Starting...");
+
+    kernel_init_main();
+
+    // disk_test();
+}
+
+void kernel_init_main() {
+    vga_writestring("Loading kernel...");
+    pmm_init(1096 * M);
+    paging_init();
+    // kheap_init(KHEAP_START, KHEAP_START + KHEAP_INITIAL_SIZE, KHEAP_MAX_ADDRESS);
+    // pci_init();
+    // ata_init();
+    vga_writestring("Kernel loaded!");
 }
 
 void disk_test(void) {
